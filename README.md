c6e9414286878d8d2fdbb7da342f175c7d147c7b
chengh <chengh@ebs.org.cn>
2018-08-23 18:00:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# music_mini_program
+
+> a music mini program
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+### 借助工具：
+
+- 一个使用 Vue.js 开发小程序的前端框架：[http://mpvue.com/](http://mpvue.com/)
+- 网易云音乐API：[https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=neteasecloudmusicapi](https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=neteasecloudmusicapi "网易云音乐API")
+- 服务器反向代理（ngrok）：[https://ngrok.com/](https://ngrok.com/ "服务器反向代理")
+- 参照作品：[https://github.com/MengZhaoFly/wechatApp-netease_cloudmusic](https://github.com/MengZhaoFly/wechatApp-netease_cloudmusic)
---
+++ src/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    首页
+    <i-button>按钮</i-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      // motto: 'Hello World',
+      // userInfo: {}
+    }
+  },
+
+  components: {},
+
+  methods: {
+    // bindViewTap () {
+    //   const url = '/packageA/logs'
+    //   wx.navigateTo({ url })
+    // },
+    // getUserInfo () {
+    //   // 调用登录接口
+    //   wx.login({
+    //     success: () => {
+    //       wx.getUserInfo({
+    //         success: (res) => {
+    //           this.userInfo = res.userInfo
+    //         }
+    //       })
+    //     }
+    //   })
+    // },
+    // clickHandle (msg, ev) {
+    //   // eslint-disable-next-line
+    //   console.log('clickHandle:', msg, ev)
+    // }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    // this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped>
+</style>
